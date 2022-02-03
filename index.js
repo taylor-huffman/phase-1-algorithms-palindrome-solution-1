a605f83992e86229bee56c8afed255bbe7723942
Taylor Huffman <taylor@crucialartists.com>
2022-02-02 21:25:04 -0600

    commit tree 0dc87b8c2e391a12837fbad18d994cf973f25b3d

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+
+  The isPalindrome() function takes in a string as an argument, creates a
+  const variable and assigns a helper function reverseSring() which takes
+  the original string as an argument.
+
+  The reverseString() function returns the string with a method chain of
+  split(), reverse(), and join().
+
+  isPalindrome then returns a true or false value when comparing the original
+  string to the reversed string.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("wow"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("people"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
